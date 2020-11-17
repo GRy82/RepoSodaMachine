c57e223dcaf698010f685569d5b7a7602dc03e83
GRy82 <gwryan@buffalo.edu>
2020-11-17 16:20:46 -0500

    FillRegister method now also completed for the WalletClass

--- SodaMachine/Wallet.cs
+++ SodaMachine/Wallet.cs
@@ -20,7 +20,18 @@ namespace SodaMachine
         //Fills wallet with starting money
         private void FillRegister()
         {
-          
+            Coin[] customerPennys = new Penny[50];
+            Coin[] customerNickels = new Nickel[10];
+            Coin[] customerDimes = new Dime[10];
+            Coin[] customerQuarters = new Quarter[12];
+            List<Coin[]> customerCoinsArrays = new List<Coin[]> { customerPennys, customerNickels, customerDimes, customerQuarters };
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < customerCoinsArrays[i].Length; j++)
+                {
+                    Coins.Add(customerCoinsArrays[i][j]);
+                }
+            }
         }
     }
 }
